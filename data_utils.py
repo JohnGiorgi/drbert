eaa3ead109005ee28f1772227d2fd77006b2a757
Nicola Sahar <nicola.sahar@gmail.com>
2019-08-26 09:40:11 -0400

    Cleaned cohort data_utils, added diabetes_data and preprocess script

--- data_utils.py
+++ data_utils.py
@@ -8,8 +8,6 @@ from torch.utils.data import TensorDataset
 
 from preprocess_cohort import read_charts, read_labels
 
-# TODO: Don't define this globally. Create it once and pass it to any function that needs it or
-# have the function that need it init it within their scope.
 nlp = spacy.load("en_core_sci_sm")
 
 CONSTANTS = {
@@ -21,46 +19,48 @@ CONSTANTS = {
 }
 
 
-def prepare_cohort_dataset(tokenizer):
+def prepare_cohort_dataset():
     base_path = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(base_path, "diabetes_data")
-
-    # charts format: test_charts[chart_id] = text # format
+    
+    #charts format: test_charts[chart_id] = text # format
     input = read_charts(data_path)
-
-    # labels format: test_labels[chart_id][disease_name] = judgement # format
-    # example label: {'Asthma': 'N', 'CHF': 'Y', 'Depression': 'N', 'Diabetes': 'Y', 'Gallstones': 'N', 'Gout': 'N', 'Hypercholesterolemia': 'N', 'Hypertriglyceridemia': 'N', 'OA': 'Y', 'OSA': 'N'}
-    # TODO: This is assigned to be never used?
+    
+    #labels format: test_labels[chart_id][disease_name] = judgement # format
     labels = read_labels(data_path)
-
+    
     documents = []
     documents_padded = []
     attention_masks = []
     doc_ids = []
-
+    
     for chart in input[0]:
+    
         documents.append(input[0][chart])
         doc_ids.append(chart)
-
-    for doc in documents:
+    
+    for doc in documents: 
         max_sent_len = 250
         doc = nlp(doc)
 
         sentence_list = [sentence for sentence in list(doc.sents)]
         token_list = [[str(token) for token in sentence] for sentence in sentence_list]
 
-        padding_len = max_sent_len - len(sentence_list)
-        pad = [CONSTANTS['PAD'] for i in range(512)]
-
-        token_list += [pad for i in range(padding_len)]
-
+        if len(sentence_list) < max_sent_len:
+            padding_len = max_sent_len - len(sentence_list)
+            pad = [CONSTANTS['PAD'] for i in range(512)]
+            token_list += [pad for i in range(padding_len)]
+        
+        else:
+            sentence_list = sentence_list[:250]
+        
         token_ids, attention_mask = index_pad_mask_bert_tokens(token_list, tokenizer)
         documents_padded.append(token_ids.unsqueeze(0))
         attention_masks.append(attention_mask.unsqueeze(0))
-
+        
     documents_padded = torch.cat(documents_padded, dim=0)
     attention_masks = torch.cat(attention_masks, dim=0)
-
+    
     return TensorDataset(documents_padded, attention_masks)
 
 
@@ -206,7 +206,3 @@ def index_pad_mask_bert_tokens(tokens,
         outputs = outputs + labels
 
     return outputs
-
-# TODO: Delete when done with.
-# document = "398811546 | TH | 30493757 | | 594476 | 9/15/2001 12:00:00 AM | CONGESTIVE HEART FAILURE | Signed | DIS | Admission Date: 9/15/2001 Report Status: Signed\n\nDischarge Date: 2/5/2001\nHISTORY OF PRESENT ILLNESS: Mr. Raffo is a 59-year-old male\nwith a history of coronary artery\ndisease status post small non-ST elevation myocardial infarction in\nOctober of 2000 and also status post cardiac catheterization with 2\nvessel disease , small left PICA cerebrovascular accident ,\ncongestive heart failure with an echocardiogram in October\nrevealing an ejection fraction of 30%. Also diabetes mellitus type\nII complicated by retinopathy , nephropathy and question neuropathy.\nHistory of hypertension , hypercholesterolemia as well who\npresented to Dr. Bouie clinic on May , 2001 with\nprogressive increasing dyspnea , increasing abdominal girth and\nweight gain over the past few months.\nPAST MEDICAL HISTORY: Diabetes mellitus x 20 years , chronic renal\ninsufficiency with the last creatinine in\nJanuary of 3.5 , congestive heart failure with an ejection fraction of\n30% in October of 2000 , coronary artery disease status post\nmyocardial infarction , October 2000 question f silent myocardial\ninfarction x 2 , history of hyperlipidemia.\nMEDICATIONS ON ADMISSION: Aspirin daily , Lasix 80 mg p.o. q day ,\nZaroxolyn 2.5 mg p.o. q day , toprol XL\n50 mg p.o. q day , insulin 70/30 65/45 , Actos 45 q p.m , Avapro 300\nmg p.o. q day , Lipitor 10 mg p.o. q.h.s. , sublingual nitroglycerin\np.r.n..\nPHYSICAL EXAM: Temperature 98.1 , blood pressure 116/70 , pulse 88 ,\nprimary care physician of 18. The patient appeared\nto be in no acute distress. Jugular venous pressure approximately\n13 cm water. LUNGS: Faint bibasilar rales. CARDIAC:\nTachycardia , regular rhythm without murmurs. ABDOMEN: Positive ,\nnormal active bowel sounds. Obese , distended and nontender.\nEXTREMITIES: 2 plus dorsalis pedis pulse , 2-3 plus edema\nbilaterally to the knees.\nHOSPITAL COURSE: 1. Cardiovascular: It was felt that given notes\nof thyroid dysfunction , his history was\nconsistent with fluid overload. His jugular venous pressure was\nelevated and he had 3plus pitting edema to the knees without\nsignificant rales on chest exam. He was aggressively diuresed with\ndoses of Lasix 200 mg b.i.d. IV as well as Zaroxolyn. Weight on\nadmission was 135 kilograms and on discharge he was down to 132\nkilograms. A repeat echocardiogram at Ethool Hospital\nshowed an ejection fraction of 30-35 , left ventricular dimensions\nof 47 mm , 1 plus mitral regurgitation and global hypokinesis as\nwell as moderate right ventricular dysfunction. Abdominal\nultrasound showed no ascites despite extremely distended abdomen\nand right renal cyst x 2. Follow up abdominal CT also showed there\nwas no mass or ascites and his distended abdomen was likely due to\nadipose tissue. Diuresis was limited by acute and chronic renal\nfailure and oliguria. Diuresis was held from May , 2001.\nRight heart catheterization on February , 2001 showed mildly\nelevated pulmonary capillary wedge pressure and moderately elevated\nright ventricular pressures. Dopamine was started on June ,\n2001 to aid with renal perfusion and diuresis and he was then\nweaned off of that on July , 2001. His Lasix was stopped on\nJuly , 2001 and he auto-diuresed until the time of discharge\nwith stable blood pressure. He remained stable with no dyspnea at\nrest and is symptom free at the time of discharge.\n2. Renal: His basic chronic renal insufficiency is likely\nsecondary to poor diabetic control with a creatinine of 2.5 on October , 2001 , 3.3 at the time of admission. Acute renal failure with\nincreasing creatinine of 6 after aggressive diuresis with a mean of\n0.8 percent. Renal function improved with creatinine of 4.1 on\nJuly , 2001 with autodiuresis. The renal service was\nconsulted to comment on the nature of the patient's renal\ninsufficiency and acute renal failure as well as to assist in the\nuse of vasodilators.\n3. Endocrine: He has a long standing history of type II diabetes\ncomplicated by retinopathy and neuropathy and nephropathy. He was\nmaintained on his outpatient endocrine regimen during his stay.\nThe patient was discharged home with services. He is to follow up\nwith Dr. Marro on November , 2001 at 3:30 p.m. , with Dr. Loatman on\nAugust , 2002 at 3:30 p.m. , Dr. Carcano January , 2001 at 10:00\na.m.\nMEDICATIONS ON DISCHARGE: Aspirin 325 mg p.o. q day , Lasix 80 mg\np.o. q day , Zocor 20 mg p.o. q.h.s. ,\ninsulin 70/30 65 units q a.m. , insulin 70/30 45 units q p.m. ,\nToprol XL 50 mg p.o. q day , Levaquin 250 mg p.o. q day for a\nduration of 7 days , Actos 45 mg p.o. q p.m.\nCONDITION ON DISCHARGE: Stable.\nDictated By: BENJAMIN DOUYON , M.D. OF45\nAttending: RUDOLF D. FEDERKEIL , M.D. YI70  OL023/102273\nBatch: 54454 Index No. D0NUNVNAP2 D: 11/2/02\nT: 11/2/02"
-# print(prepare_cohort_dataset())
---
+++ preprocess_cohort.py
@@ -0,0 +1,122 @@
+import xmltodict
+import json
+import os
+
+#print("hello from the other side")
+
+def read_charts(data_path="diabetes_data"):
+
+	train_charts = dict()
+	train_file_list = [
+		os.path.join(data_path, "obesity_patient_records_training.xml"),
+		os.path.join(data_path, "obesity_patient_records_training2.xml")
+	]
+	for train_file in train_file_list:
+		with open(train_file) as open_file:
+			read_file = open_file.read()
+			train_data1 = dict(xmltodict.parse(read_file))
+			for i in range(len(train_data1['root']['docs']['doc'])):
+				chart_id = train_data1['root']['docs']['doc'][i]['@id']
+				text = train_data1['root']['docs']['doc'][i]['text']
+				train_charts[chart_id] = text # format
+
+	test_charts = dict()
+	test_file = os.path.join(data_path, "obesity_patient_records_test.xml")
+	with open(test_file) as open_file:
+		read_file = open_file.read()
+		train_data1 = dict(xmltodict.parse(read_file))
+		for i in range(len(train_data1['root']['docs']['doc'])):
+			chart_id = train_data1['root']['docs']['doc'][i]['@id']
+			text = train_data1['root']['docs']['doc'][i]['text']
+			test_charts[chart_id] = text # format
+
+	return train_charts, test_charts
+
+def read_labels(data_path="diabetes_data"):
+	train_labels = dict()
+	addendum_file = os.path.join(data_path, "obesity_standoff_annotations_training.xml")
+	with open(addendum_file) as open_file:
+		read_file = open_file.read()
+		label_data = dict(xmltodict.parse(read_file))
+		for i in range(len(label_data['diseaseset']['diseases'])):
+			if label_data['diseaseset']['diseases'][i]['@source'] != "intuitive":
+				continue
+			print(f"{addendum_file}: {label_data['diseaseset']['diseases'][i]['@source']}")
+			for j in range(len(label_data['diseaseset']['diseases'][i]['disease'])):
+				disease_name = label_data['diseaseset']['diseases'][i]['disease'][j]['@name']
+				for k in range(len(label_data['diseaseset']['diseases'][i]['disease'][j]['doc'])):
+					chart_id = label_data['diseaseset']['diseases'][i]['disease'][j]['doc'][k]['@id']
+					judgement = label_data['diseaseset']['diseases'][i]['disease'][j]['doc'][k]['@judgment']
+					if chart_id not in train_labels:
+						train_labels[chart_id] = dict()
+					
+					# print(f"{chart_id}, {disease_name}: {judgement}")
+					train_labels[chart_id][disease_name] = judgement # format
+
+	addendum_file = os.path.join(data_path, "obesity_standoff_annotations_training_addendum.xml")
+	with open(addendum_file) as open_file:
+		read_file = open_file.read()
+		label_data = dict(xmltodict.parse(read_file))
+		print(f"{addendum_file}: {label_data['diseaseset']['diseases']['@source']}")
+		disease_name = label_data['diseaseset']['diseases']['disease']['@name']
+		for i in range(len(label_data['diseaseset']['diseases']['disease']['doc'])):
+			chart_id = label_data['diseaseset']['diseases']['disease']['doc'][i]['@id']
+			judgement = label_data['diseaseset']['diseases']['disease']['doc'][i]['@judgment']
+			if chart_id not in train_labels:
+				train_labels[chart_id] = dict()
+			if disease_name in train_labels[chart_id]:
+				print("uh oh.")
+			train_labels[chart_id][disease_name] = judgement # format
+
+	addendum_list = [
+		# os.path.join(data_path, "obesity_standoff_annotations_training_addendum2.xml"),
+		os.path.join(data_path, "obesity_standoff_annotations_training_addendum3.xml")
+	]
+
+	for addendum_file in addendum_list:
+		with open(addendum_file) as open_file:
+			read_file = open_file.read()
+			label_data = dict(xmltodict.parse(read_file))
+			for i in range(len(label_data['diseaseset']['diseases'])):
+				if label_data['diseaseset']['diseases'][i]['@source'] != "intuitive":
+					continue
+				print(f"{addendum_file}: {label_data['diseaseset']['diseases'][i]['@source']}")
+
+				for j in range(len(label_data['diseaseset']['diseases'][i]['disease'])):
+					disease_name = label_data['diseaseset']['diseases'][i]['disease'][j]['@name']
+					docs = label_data['diseaseset']['diseases'][i]['disease'][j]['doc']
+					for doc in docs:
+						chart_id = doc['@id']
+						judgement = doc['@judgment']
+						if chart_id not in train_labels:
+							train_labels[chart_id] = dict()
+						if disease_name in train_labels[chart_id]:
+							print("uh oh.")
+						train_labels[chart_id][disease_name] = judgement # format
+
+	test_labels = dict()
+	test_file = os.path.join(data_path, "obesity_standoff_annotations_test_intuitive.xml")
+	with open(test_file) as open_file:
+		read_file = open_file.read()
+		label_data = dict(xmltodict.parse(read_file))
+		print(f"{test_file}: {label_data['diseaseset']['diseases']['@source']}")
+		for i in range(len(label_data['diseaseset']['diseases']['disease'])):
+			disease_name = label_data['diseaseset']['diseases']['disease'][i]['@name']
+			for j in range(len(label_data['diseaseset']['diseases']['disease'][i]['doc'])):
+				chart_id = label_data['diseaseset']['diseases']['disease'][i]['doc'][j]['@id']
+				judgement = label_data['diseaseset']['diseases']['disease'][i]['doc'][j]['@judgment']
+				if chart_id not in test_labels:
+					test_labels[chart_id] = dict()
+				
+				# print(f"{chart_id}, {disease_name}: {judgement}")
+				test_labels[chart_id][disease_name] = judgement # format
+	return train_labels, test_labels
+
+train_charts, test_charts = read_charts()
+train_labels, test_labels = read_labels()
+for chart_id, _ in train_charts.items():
+	if chart_id not in train_labels:
+		print(f"{chart_id} not in ")
+for chart_id, _ in test_charts.items():
+	if chart_id not in test_labels:
+		print(f"{chart_id} not in ")
