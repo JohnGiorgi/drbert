994f34ff51feba4d62a93df8e804b45fc1634673
Gary Shin <garyshincc@gmail.com>
2019-08-28 15:44:01 -0400

    changed cohort preprocess from intuitive to textual

--- preprocess_cohort.py
+++ preprocess_cohort.py
@@ -5,7 +5,7 @@ import os
 #print("hello from the other side")
 # TODO, CHANGET INTUITIVE TO TEXTUAL
 
-def read_charts(data_path="diabetes_data"):
+def read_charts(data_path="data/diabetes_data"):
 
 	train_charts = dict()
 	train_file_list = [
@@ -33,14 +33,14 @@ def read_charts(data_path="diabetes_data"):
 
 	return train_charts, test_charts
 
-def read_labels(data_path="diabetes_data"):
+def read_labels(data_path="data/diabetes_data"):
 	train_labels = dict()
 	addendum_file = os.path.join(data_path, "obesity_standoff_annotations_training.xml")
 	with open(addendum_file) as open_file:
 		read_file = open_file.read()
 		label_data = dict(xmltodict.parse(read_file))
 		for i in range(len(label_data['diseaseset']['diseases'])):
-			if label_data['diseaseset']['diseases'][i]['@source'] != "intuitive":
+			if label_data['diseaseset']['diseases'][i]['@source'] != "textual":
 				continue
 			print(f"{addendum_file}: {label_data['diseaseset']['diseases'][i]['@source']}")
 			for j in range(len(label_data['diseaseset']['diseases'][i]['disease'])):
@@ -50,53 +50,59 @@ def read_labels(data_path="diabetes_data"):
 					judgement = label_data['diseaseset']['diseases'][i]['disease'][j]['doc'][k]['@judgment']
 					if chart_id not in train_labels:
 						train_labels[chart_id] = dict()
-					
-					# print(f"{chart_id}, {disease_name}: {judgement}")
 					train_labels[chart_id][disease_name] = judgement # format
 
-	addendum_file = os.path.join(data_path, "obesity_standoff_annotations_training_addendum.xml")
+	addendum_file = os.path.join(data_path, "obesity_standoff_annotations_training_addendum2.xml")
 	with open(addendum_file) as open_file:
 		read_file = open_file.read()
 		label_data = dict(xmltodict.parse(read_file))
 		print(f"{addendum_file}: {label_data['diseaseset']['diseases']['@source']}")
-		disease_name = label_data['diseaseset']['diseases']['disease']['@name']
-		for i in range(len(label_data['diseaseset']['diseases']['disease']['doc'])):
-			chart_id = label_data['diseaseset']['diseases']['disease']['doc'][i]['@id']
-			judgement = label_data['diseaseset']['diseases']['disease']['doc'][i]['@judgment']
-			if chart_id not in train_labels:
-				train_labels[chart_id] = dict()
-			if disease_name in train_labels[chart_id]:
-				print("uh oh.")
-			train_labels[chart_id][disease_name] = judgement # format
+		disease_label_list = label_data['diseaseset']['diseases']['disease']
+		for disease_label in disease_label_list:
+			disease_name = disease_label['@name']
+			disease_docs = disease_label['doc']
+			if type(disease_docs) == list:
+				for doc in disease_docs:
+					chart_id = doc['@id']
+					chart_label = doc['@judgment']
+					if chart_id not in train_labels:
+						train_labels[chart_id] = dict()
+					if disease_name in train_labels[chart_id]:
+						print("uh oh.")
+					train_labels[chart_id][disease_name] = chart_label # format
+			else:
+				doc = disease_docs
+				chart_id = doc['@id']
+				chart_label = doc['@judgment']
+				if chart_id not in train_labels:
+					train_labels[chart_id] = dict()
+				if disease_name in train_labels[chart_id]:
+					print("uh oh.")
+				train_labels[chart_id][disease_name] = chart_label # format
 
-	addendum_list = [
-		# os.path.join(data_path, "obesity_standoff_annotations_training_addendum2.xml"),
-		os.path.join(data_path, "obesity_standoff_annotations_training_addendum3.xml")
-	]
+	addendum_file = os.path.join(data_path, "obesity_standoff_annotations_training_addendum3.xml")
+	with open(addendum_file) as open_file:
+		read_file = open_file.read()
+		label_data = dict(xmltodict.parse(read_file))
+		for i in range(len(label_data['diseaseset']['diseases'])):
+			if label_data['diseaseset']['diseases'][i]['@source'] != "textual":
+				continue
+			print(f"{addendum_file}: {label_data['diseaseset']['diseases'][i]['@source']}")
 
-	for addendum_file in addendum_list:
-		with open(addendum_file) as open_file:
-			read_file = open_file.read()
-			label_data = dict(xmltodict.parse(read_file))
-			for i in range(len(label_data['diseaseset']['diseases'])):
-				if label_data['diseaseset']['diseases'][i]['@source'] != "intuitive":
-					continue
-				print(f"{addendum_file}: {label_data['diseaseset']['diseases'][i]['@source']}")
-
-				for j in range(len(label_data['diseaseset']['diseases'][i]['disease'])):
-					disease_name = label_data['diseaseset']['diseases'][i]['disease'][j]['@name']
-					docs = label_data['diseaseset']['diseases'][i]['disease'][j]['doc']
-					for doc in docs:
-						chart_id = doc['@id']
-						judgement = doc['@judgment']
-						if chart_id not in train_labels:
-							train_labels[chart_id] = dict()
-						if disease_name in train_labels[chart_id]:
-							print("uh oh.")
-						train_labels[chart_id][disease_name] = judgement # format
+			for j in range(len(label_data['diseaseset']['diseases'][i]['disease'])):
+				disease_name = label_data['diseaseset']['diseases'][i]['disease'][j]['@name']
+				docs = label_data['diseaseset']['diseases'][i]['disease'][j]['doc']
+				for doc in docs:
+					chart_id = doc['@id']
+					judgement = doc['@judgment']
+					if chart_id not in train_labels:
+						train_labels[chart_id] = dict()
+					if disease_name in train_labels[chart_id]:
+						print("uh oh.")
+					train_labels[chart_id][disease_name] = judgement # format
 
 	test_labels = dict()
-	test_file = os.path.join(data_path, "obesity_standoff_annotations_test_intuitive.xml")
+	test_file = os.path.join(data_path, "obesity_standoff_annotations_test_textual.xml")
 	with open(test_file) as open_file:
 		read_file = open_file.read()
 		label_data = dict(xmltodict.parse(read_file))
@@ -108,17 +114,15 @@ def read_labels(data_path="diabetes_data"):
 				judgement = label_data['diseaseset']['diseases']['disease'][i]['doc'][j]['@judgment']
 				if chart_id not in test_labels:
 					test_labels[chart_id] = dict()
-				
-				# print(f"{chart_id}, {disease_name}: {judgement}")
 				test_labels[chart_id][disease_name] = judgement # format
 	return train_labels, test_labels
 
-# train_charts, test_charts = read_charts()
-# train_labels, test_labels = read_labels()
-# for chart_id, _ in train_charts.items():
-# 	if chart_id not in train_labels:
-# 		print(f"{chart_id} not in ")
-# for chart_id, _ in test_charts.items():
-# 	if chart_id not in test_labels:
-# 		print(f"{chart_id} not in ")
+train_charts, test_charts = read_charts("data/diabetes_data")
+train_labels, test_labels = read_labels("data/diabetes_data")
+for chart_id, _ in train_charts.items():
+	if chart_id not in train_labels:
+		print(f"{chart_id} not in ")
+for chart_id, _ in test_charts.items():
+	if chart_id not in test_labels:
+		print(f"{chart_id} not in ")
 
