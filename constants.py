539cce6940d5d125607e723a91f92205b55316d0
Gary Shin <garyshincc@gmail.com>
2019-08-29 16:56:18 -0400

    single constants file

---
+++ constants.py
@@ -0,0 +1,37 @@
+
+CONSTANTS = {
+    'SEP': '[SEP]',
+    'CLS': '[CLS]',
+    'UNK': '[UNK]',
+    'PAD': '[PAD]',
+    'TOK_MAP_PAD': -100,
+    'WORDPIECE': 'X'
+}
+
+COHORT_LABEL_CONSTANTS = {
+    'U': 0,
+    'Q': 1,
+    'N': 2,
+    'Y': 3,
+}
+
+COHORT_DISEASE_CONSTANTS = {
+    'Obesity': 0,
+    'Diabetes': 1,
+    'Hypercholesterolemia': 2,
+    'Hypertriglyceridemia': 3,
+    'Hypertension': 4,
+    'CAD': 5, 
+    'CHF': 6,
+    'PVD': 7,
+    'Venous Insufficiency': 8,
+    'OA': 9,
+    'OSA': 10,
+    'Asthma': 11,
+    'GERD': 12,
+    'Gallstones': 13,
+    'Depression': 14,
+    'Gout': 15
+}
+
+MAX_COHORT_NUM_SENTS = 256 # number of sentences in chart
--- data_utils.py
+++ data_utils.py
@@ -11,46 +11,11 @@ from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
                               TensorDataset)
 
 from preprocess_cohort import read_charts, read_labels
+from constants import *
 from pytorch_transformers import BertTokenizer
 
 
 
-CONSTANTS = {
-    'SEP': '[SEP]',
-    'CLS': '[CLS]',
-    'UNK': '[UNK]',
-    'PAD': '[PAD]',
-    'TOK_MAP_PAD': -100,
-    'WORDPIECE': 'X'
-}
-
-COHORT_LABEL_CONSTANTS = {
-    'U': 0,
-    'Q': 1,
-    'N': 2,
-    'Y': 3,
-}
-
-COHORT_DISEASE_CONSTANTS = {
-    'Obesity': 0,
-    'Diabetes': 1,
-    'Hypercholesterolemia': 2,
-    'Hypertriglyceridemia': 3,
-    'Hypertension': 4,
-    'CAD': 5, 
-    'CHF': 6,
-    'PVD': 7,
-    'Venous Insufficiency': 8,
-    'OA': 9,
-    'OSA': 10,
-    'Asthma': 11,
-    'GERD': 12,
-    'Gallstones': 13,
-    'Depression': 14,
-    'Gout': 15
-}
-
-MAX_COHORT_NUM_SENTS = 256 # number of sentences in chart
 
 class CohortDataset(torch.utils.data.Dataset):
     def __init__(self, data_path):
