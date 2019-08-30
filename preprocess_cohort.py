a8ec712ec667883281d75906d732f5976565b656
Gary Shin <gary@nanoleaf.me>
2019-08-30 10:31:07 -0400

    put more contents in to constants

--- preprocess_cohort.py
+++ preprocess_cohort.py
@@ -1,51 +1,8 @@
 import xmltodict
 import json
 import os
+from constants import *
 
-COHORT_LABEL_CONSTANTS = {
-    'U': 0,
-    'Q': 1,
-    'N': 2,
-    'Y': 3,
-}
-
-COHORT_DISEASE_LIST = [
-    'Obesity',
-    'Diabetes',
-    'Hypercholesterolemia',
-    'Hypertriglyceridemia',
-    'Hypertension',
-    'CAD',
-    'CHF',
-    'PVD',
-    'Venous Insufficiency',
-    'OA',
-    'OSA',
-    'Asthma',
-    'GERD',
-    'Gallstones',
-    'Depression',
-    'Gout'
-]
-
-COHORT_DISEASE_CONSTANTS = {
-    'Obesity': 0,
-    'Diabetes': 1,
-    'Hypercholesterolemia': 2,
-    'Hypertriglyceridemia': 3,
-    'Hypertension': 4,
-    'CAD': 5, 
-    'CHF': 6,
-    'PVD': 7,
-    'Venous Insufficiency': 8,
-    'OA': 9,
-    'OSA': 10,
-    'Asthma': 11,
-    'GERD': 12,
-    'Gallstones': 13,
-    'Depression': 14,
-    'Gout': 15
-}
 
 def read_charts(data_path="data/diabetes_data"):
 
