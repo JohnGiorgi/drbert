efa6faeb982e3b3ca721a659b00b07219227f5f4
johngiorgi <johnmgiorgi@gmail.com>
2019-08-30 11:26:14 -0400

    :sparkles: Add eval logic for seq lab problems

---
+++ eval_utils.py
@@ -0,0 +1,55 @@
+from seqeval.metrics import f1_score
+from seqeval.metrics import precision_score
+from seqeval.metrics import recall_score
+from seqeval.metrics.sequence_labeling import get_entities
+
+
+def precision_recall_f1_support_sequence_labelling(y_true, y_pred, criteria='exact'):
+    """Compute precision, recall, f1 and support for sequence labelling tasks.
+
+    For given gold (`y_true`) and predicted (`y_pred`) sequence labels, returns the precision,
+    recall, f1 and support per label, and the macro and micro average of these scores across
+    labels. Expects `y_true` and `y_pred` to be a sequence of IOB1/2, IOE1/2, or IOBES formatted
+    labels.
+
+    Args:
+        y_true (list): List of IOB1/2, IOE1/2, or IOBES formatted sequence labels.
+        y_pred (list): List of IOB1/2, IOE1/2, or IOBES formatted sequence labels.
+
+    Returns:
+        A dictionary of scores keyed by the labels in `y_true` where each score is a 4-tuple
+        containing precision, recall, f1 and support. Additionally includes the keys
+        'Macro avg' and 'Micro avg' containing the macro and micro averages across scores.
+    """
+    scores = {}
+    # Unique labels, not including NEG
+    labels = list({tag.split('-')[-1] for tag in set(y_true) if tag != OUTSIDE})
+    labels.sort()  # ensures labels displayed in same order across runs / partitions
+
+    for label in labels:
+        y_true_lab = [tag if tag.endswith(label) else OUTSIDE for tag in y_true]
+        y_pred_lab = [tag if tag.endswith(label) else OUTSIDE for tag in y_pred]
+
+        # TODO (John): Open a pull request to seqeval with a new function that returns all these
+        # scores in one call. There is a lot of repeated computation here.
+        precision = precision_score(y_true_lab, y_pred_lab, criteria=criteria)
+        recall = recall_score(y_true_lab, y_pred_lab, criteria=criteria)
+        f1 = f1_score(y_true_lab, y_pred_lab, criteria=criteria)
+        support = len(set(get_entities(y_true_lab)))
+
+        scores[label] = precision, recall, f1, support
+
+    # Get macro and micro performance metrics averages
+    macro_precision = mean([v[0] for v in scores.values()])
+    macro_recall = mean([v[1] for v in scores.values()])
+    macro_f1 = mean([v[2] for v in scores.values()])
+    total_support = sum([v[3] for v in scores.values()])
+
+    micro_precision = precision_score(y_true, y_pred, criteria=criteria)
+    micro_recall = recall_score(y_true, y_pred, criteria=criteria)
+    micro_f1 = f1_score(y_true, y_pred, criteria=criteria)
+
+    scores['Macro avg'] = macro_precision, macro_recall, macro_f1, total_support
+    scores['Micro avg'] = micro_precision, micro_recall, micro_f1, total_support
+
+    return scores
