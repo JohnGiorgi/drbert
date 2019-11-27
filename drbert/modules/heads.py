5b21742ac31627ffd546ab10a2b24ee1619592a2
johngiorgi <johnmgiorgi@gmail.com>
2019-11-26 19:00:36 -0500

    :construction: dice loss for seq class tasks

--- drbert/modules/heads.py
+++ drbert/modules/heads.py
@@ -2,6 +2,9 @@ import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss
 from torch.nn import MSELoss
+from torch.nn.functional import one_hot
+
+from .dice_loss import DiceLoss
 
 
 class SequenceLabellingHead(torch.nn.Module):
@@ -126,8 +129,11 @@ class SequenceClassificationHead(torch.nn.Module):
                 loss_fct = MSELoss()
                 loss = loss_fct(logits.view(-1), labels.view(-1))
             else:
-                loss_fct = CrossEntropyLoss()
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+                loss_fct = DiceLoss()
+                loss = loss_fct(
+                    predict=logits.view(-1, self.num_labels),
+                    target=one_hot(labels.view(-1), self.num_labels).float()
+                )
             outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
